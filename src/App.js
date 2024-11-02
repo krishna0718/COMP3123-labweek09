36f02cbdc30f261e04372bf92bfcc0dd74eab045
krish <krishnathkkar072004@gmail.com>
2024-11-01 20:21:04 -0400

    Lab excercise week09

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <h1>Welcome to FullStack development -I </h1>
+          <h2>React JS programming lab exercise 09</h2>
+          <p>Student ID :- 101398578</p>
+          <p>Name:- Krishna Thakkar</p>
+          <a className='George-brown' href='https://www.georgebrown.ca/' target='_blank'>George Brown College, Toronto</a>
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
